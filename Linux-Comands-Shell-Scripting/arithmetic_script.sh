74e3450546510ae4449db1d08e907b559b761142
kateswim <goncharovakiev@gmail.com>
2025-11-09 21:11:44 -0800

    updates

--- Linux-Comands-Shell-Scripting/arithmetic_script.sh
+++ Linux-Comands-Shell-Scripting/arithmetic_script.sh
@@ -14,3 +14,14 @@ product=$(($integer1*$integer2))
 
 echo "The sum is $sum"
 echo "The product is $product"
+
+if [ $sum -lt $product ]
+then
+   echo "The sum is less than the product."
+elif [ $sum -eq $product ]
+then
+   echo "The sum is equal to the product."
+elif [ $sum -gt $product ]
+then
+   echo "The sum is greater than the product."
+fi
