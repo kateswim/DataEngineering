6f997d9e26bcaf57f3ea2e3070530d9285a0076f
kateswim <goncharovakiev@gmail.com>
2025-11-10 23:10:34 -0800

    updates

--- Linux-Comands-Shell-Scripting/arrays_table.sh
+++ Linux-Comands-Shell-Scripting/arrays_table.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-csv_file="https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-LX0117EN-SkillsNetwork/labs/M3/L2/arrays_table.csv"
-wget $csv_file
+csv_url="https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-LX0117EN-SkillsNetwork/labs/M3/L2/arrays_table.csv"
+data_file="arrays_table.csv"
+curl -L "$csv_url" -o "$data_file"
 
 # parse table columns into 3 arrays
-column_0=($(cut -d "," -f 1 $csv_file))
-column_1=($(cut -d "," -f 2 $csv_file))
-column_2=($(cut -d "," -f 3 $csv_file))
+column_0=($(cut -d "," -f 1 "$data_file"))
+column_1=($(cut -d "," -f 2 "$data_file"))
+column_2=($(cut -d "," -f 3 "$data_file"))
 
 # print first array
 echo "Displaying the first column:"
@@ -16,7 +17,7 @@ echo "${column_0[@]}"
 # initialize array with header
 column_3=("column_3")
 # get the number of lines in each column
-nlines=$(cat $csv_file | wc -l)
+nlines=$(wc -l < "$data_file")
 echo "There are $nlines lines in the file"
 # populate the array
 for ((i=1; i<$nlines; i++)); do
@@ -32,4 +33,4 @@ echo "${column_3[0]}" > column_3.txt
 for ((i=1; i<nlines; i++)); do
   echo "${column_3[$i]}" >> column_3.txt
 done
-paste -d "," $csv_file column_3.txt > report.csv
+paste -d "," "$data_file" column_3.txt > report.csv
