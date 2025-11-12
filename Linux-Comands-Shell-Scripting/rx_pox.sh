49fb4295f04e56e8a7b4f4aec7c41dba6b496dcb
kateswim <goncharovakiev@gmail.com>
2025-11-11 19:16:54 -0800

    Create rx_pox.sh

---
+++ Linux-Comands-Shell-Scripting/rx_pox.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+city=Surrey
+
+curl -s "wttr.in/$city?T" --output weather_report
+
+#To extract Current Temperature
+obs_temp=$(grep -m 1 '°.' weather_report | grep -Eo -e '-?[[:digit:]].*')
+
+echo "The current Temperature of $city: $obs_temp"
+
+fc_temp=$(head -23 weather_report | tail -1 | grep '°.' | cut -d 'C' -f2 | grep -Eo -e '-?[[:digit:]].*')
+
+#Assign Country and City to variable TZ
+TZ='Canada/Surrey'
+
+# Use command substitution to store the current day, month, and year in corresponding shell variables:
+day=$(TZ='Canada/Surrey' date -u +%d) 
+month=$(TZ='Canada/Surrey' date +%m)
+year=$(TZ='Canada/Surrey' date +%Y)
+
+
+record=$(echo -e "$year\t$month\t$day\t$obs_temp\t$fc_temp C")
+echo $record>>rx_poc.log
